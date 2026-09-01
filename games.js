0268c8fb6eb83126639fb0cc320e7331b41e3a2b
The boom <theboom4real@gmail.com>
2026-09-02 06:47:29 +1000

    Update games.js

--- games.js
+++ games.js
@@ -12,7 +12,7 @@ const GAMES = [
   slug: "flightsim",
   description: "fly a plane in an infinite world",
   color: "teal"   // optional — "clay" | "moss" | "teal"
-  image: ""
+  image: "IMG_2490.jpeg"
 },
 
 {
